19235c48637d3cef5a773d6f94b16070d88074f7
Joseph Wright <joe@joegle.com>
2017-09-01 00:02:11 -0400

    add word command and store in config directory

--- spelling-game.py
+++ spelling-game.py
@@ -3,35 +3,70 @@
 import argparse
 import sys
 import signal
+import os
 
 class game():
-    def __init__(self, args):
+    def __init__(self, parsed_args):
         print "init"
+        self.args = parsed_args
+        self.wordlist_file = "word-list.txt"
         self.wordlist = self.load()
+
         signal.signal(signal.SIGINT, self.on_quit)
-        
+
     def start(self):
         print "started"
 
-            
+        print self.args.command
+        if self.args.command:
+            command = self.args.command
+
+            if command[0] == "add":
+                print "adding word"
+
+                self.add_word(command[1])
+                sys.exit(0)
+
+            if command[0] == "rm":
+                print "removing word"
+                sys.exit(0)
+
         for word in self.wordlist:
             prompt = "\n$ %s\n> " % (word)
-            
+
             answer = raw_input(prompt)
 
             if answer == word:
                 print "CORRECT!"
-                    
+
+
+    def add_word(self, word):
+        file_name = self.args.config + '/word-list.txt'
+        word_list = open(file_name,'a+')
+        word_list.write(word + "\n")
+        word_list.close()
 
     def load(self):
-        f = open('word-list.txt','r')
+        print "loading config"
+
+        print "using config folder: %s" % (self.args.config)
+        if not os.path.isdir(self.args.config):
+            os.mkdir(self.args.config)
+
+        file_name = self.args.config + '/word-list.txt'
+
+        try:
+            f = open(file_name, 'r')
+        except IOError:
+            f = open(file_name, 'w')
+
         r1 = map(str.rstrip, f.readlines())
         return map(lambda x:x.lower(), r1)
 
     def on_quit(self, signal, frame):
         print "Good bye"
         sys.exit(0)
-        
+
 guide = """# Examples
 - examples 
 - here
@@ -46,9 +81,9 @@ parser.add_argument('-t','--test', help="Test run", action='store_true')
 parser.add_argument('-a','--add', help="add word to game", action='store_true')
 parser.add_argument('-r','--remove', help="remove word from game",
                     action='store_true')
-parser.add_argument('-c','--config', help="config folder")
+parser.add_argument('-c','--config', help="config folder", default=".spelling/")
 
-parser.add_argument('command', type=str, nargs='?', help="'add' or 'rm'")
+parser.add_argument('command', type=str, nargs='*', help="'add' or 'rm'")
 
 args = parser.parse_args()
 
@@ -59,4 +94,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
