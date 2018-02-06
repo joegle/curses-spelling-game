d699ecbe949dcc54e7c96c26d250876e4da214c4
Joseph Wright <joegle@gmail.com>
2018-02-05 20:37:35 -0500

    add logo panel function

--- engine/game_view.py
+++ engine/game_view.py
@@ -29,11 +29,34 @@ class GameView():
     def run(self):
         self.echo_bar("test")
         self.main_window.show()
+        self.logo_panel()
         c = self.main_window.window().getch()
         
     def __str__(self):
         return "game view"
-    
+
+    def logo_panel(self):
+        """create panel for the home screen and bring to front"""
+        screen_y, screen_x = self.screen.getmaxyx()
+        win = curses.newwin(screen_y, screen_x, 0, 0)
+
+        self.start_panel = panel.new_panel(win)
+        f = open("logo.txt", 'r').read()
+        self.start_panel.window().addstr(f, curses.A_BOLD)
+        logo_height = len(f.split("\n"))
+        start_msg = "Press any key to start"
+        self.start_panel.window().addstr(logo_height, 2, start_msg, curses.A_BOLD)
+        self.start_panel.top()
+        self.main_window.bottom()
+
+        self.pflush()
+
+    def pflush(self):
+        """refresh the panels"""
+
+        panel.update_panels()
+        curses.doupdate()
+
 def main(stdscr):
     print stdscr
     view = GameView(stdscr)
